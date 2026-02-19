4210ceafa2842a174e0cae32d4c1a8387a49c6cf
ShoeboxTheCat <79722642+ShoeboxTheCat@users.noreply.github.com>
2026-02-18 19:27:57 -0800

    Added Introduction
    added introduction from beginning ofo course discussion

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Ronit Sengupta" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("HI! I'm Ronit, I'm a junior from California. A fun fact about me is I like to build props and costumes, and I also enjoy singing and playing D&D. I look forward to learning about engineering through this and the other courses!")
 
 if __name__ == "__main__":
     main()
